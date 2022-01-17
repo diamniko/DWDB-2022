f21070a3dc1edabc2ca90a1a769ec197871ec128
diamniko <56235463+diamniko@users.noreply.github.com>
2022-01-17 19:59:09 +0200

    Σχεδόν έτοιμο!

--- src/main/scala/FindAnomalies.scala
+++ src/main/scala/FindAnomalies.scala
@@ -1,36 +1,19 @@
-import javafx.scene.chart.ScatterChart
-import org.apache.parquet.format.Util
 import org.apache.spark.ml.clustering.KMeans
-import org.apache.spark.ml.clustering.KMeansModel
 import org.apache.spark.ml.evaluation.ClusteringEvaluator
-import org.apache.spark.ml.feature.{MinMaxScaler, Normalizer, VectorAssembler}
+import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.ml.linalg.Vectors
-import org.apache.spark.mllib.util.MLUtils
-import org.apache.spark.sql.functions.{col, column, lit, max, min, pow, shuffle, sqrt}
-import org.apache.spark.sql.types.{FloatType, IntegerType}
-import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.functions._
-import org.apache.spark.ml.linalg.Vector
-import org.apache.spark.ml.recommendation.ALS.Rating
-import org.apache.spark.sql.Row
-
-import scala.io.Source
 
 object FindAnomalies {
   def main(args: Array[String]): Unit = {
-    println("***********************************************************************************************")
-    println("***********************************************************************************************")
-    println("Hello, Spark!")
+    var now = System.nanoTime()
 
     val spark = SparkSession.builder().appName("kMeansProject").master("local[2]").getOrCreate()
 
     var df = spark.read.options(Map("delimiter"->","))
       .csv("data-example2122-pps.txt")
-    df.show()
 
     df = df.na.drop()
-    df.show()
 
     var cdf = df.select(df.columns.map{
       case column@"_c0" =>
@@ -50,7 +33,6 @@ object FindAnomalies {
 
     cdf = cdf.withColumn("_c0", (col("_c0")- minC0) / (maxC0 - minC0))
     cdf = cdf.withColumn("_c1", (col("_c1")- minC1) / (maxC1 - minC1))
-    cdf.show()
 
     val cols = Array("_c0", "_c1")
     val assembler = new VectorAssembler().setInputCols(cols).setOutputCol("features")
@@ -61,44 +43,35 @@ object FindAnomalies {
 
     // Make predictions
     var predictions = model.transform(featureDf)
-    predictions.show()
 
     // Evaluate clustering by computing Silhouette score
     val evaluator = new ClusteringEvaluator()
 
     val silhouette = evaluator.evaluate(predictions)
-    println(s"Silhouette with squared euclidean distance = $silhouette")
-    println("***************************************************")
-    // Shows the result.
-    println("Cluster Centers: ")
-    model.clusterCenters.foreach(println)
 
     predictions = predictions.sort("prediction")
-    predictions.show()
 
-    predictions = predictions.select(df.columns.map{
-      case column@"prediction" =>
-        col(column).cast("Int").as(column)
-    }: _*)
-
-    predictions = predictions.withColumn("distance", lit(sqrt(pow(col("_c0") - model.clusterCenters(predictions.map(r => Rating(r.getAs[Int]("prediction"))).toArray(0), 2) + pow(col("_c1") - model.clusterCenters().toArray(1), 2))))
+    predictions = predictions.withColumn("distance",
+      when(col("prediction")===0, sqrt(pow(col("_c0") - model.clusterCenters(0).toArray(0), 2) + pow(col("_c1") - model.clusterCenters(0).toArray(1), 2)))
+     .when(col("prediction")===1, sqrt(pow(col("_c0") - model.clusterCenters(1).toArray(0), 2) + pow(col("_c1") - model.clusterCenters(1).toArray(1), 2)))
+     .when(col("prediction")===2, sqrt(pow(col("_c0") - model.clusterCenters(2).toArray(0), 2) + pow(col("_c1") - model.clusterCenters(2).toArray(1), 2)))
+     .when(col("prediction")===3, sqrt(pow(col("_c0") - model.clusterCenters(3).toArray(0), 2) + pow(col("_c1") - model.clusterCenters(3).toArray(1), 2)))
+     .when(col("prediction")===4, sqrt(pow(col("_c0") - model.clusterCenters(4).toArray(0), 2) + pow(col("_c1") - model.clusterCenters(4).toArray(1), 2)))
+    )
     val avgDistance = predictions.groupBy("prediction").agg(avg(col("distance")))
-    val divDistance = distance.withColumn("divDistance", (col("distance")/col("avgDistance")))
 
-    divDistance.filter(col("divDistance") > 1.5).show(false)
-
-    println("***********************************************************************************************")
-    println("***********************************************************************************************")
-    println("Data parsed!")
-
-    println("***********************************************************************************************")
-    println("***********************************************************************************************")
-
-
+    val divDistance = predictions.withColumn("divDistance",
+      when(col("prediction")===0, col("distance") / avgDistance.take(1)(0).getAs[Float]("avg(distance)"))
+     .when(col("prediction")===1, col("distance") / avgDistance.take(2)(0).getAs[Float]("avg(distance)"))
+     .when(col("prediction")===2, col("distance") / avgDistance.take(3)(0).getAs[Float]("avg(distance)"))
+     .when(col("prediction")===3, col("distance") / avgDistance.take(4)(0).getAs[Float]("avg(distance)"))
+     .when(col("prediction")===4, col("distance") / avgDistance.take(5)(0).getAs[Float]("avg(distance)"))
+    )
+    println("Outliers:")
+    divDistance.filter(col("divDistance") > 2.4).show()
 
     spark.stop()
-    println("Spark stopped!")
-    println("***********************************************************************************************")
-    println("***********************************************************************************************")
+    var timeElapsed = System.nanoTime() - now
+    println("Time elapsed: " + (timeElapsed/1000000000) + " sec.")
   }
 }
